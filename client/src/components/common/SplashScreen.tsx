0a7d84ed81ec436061d4c5ccbf4a6987f74eb23e
David <david.lytikainen@gmail.com>
2025-06-02 16:19:36 -0400

    update splash screen animation

--- client/src/components/common/SplashScreen.tsx
+++ client/src/components/common/SplashScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { Box, useTheme } from '@mui/material';
-import { useSpring, animated, useTrail } from '@react-spring/web';
+import { animated, useTrail } from '@react-spring/web';
 
 interface SplashScreenProps {
   type: 'login' | 'logout';
@@ -15,25 +15,19 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ type }) => {
 
   const chars = useMemo(() => message.split(''), [message]);
   const trail = useTrail(chars.length, {
-    from: { opacity: 0, transform: 'translateY(5px)' },
+    from: { opacity: 0, transform: 'translateY(0px)' },
     to: { opacity: 1, transform: 'translateY(0px)' },
     config: { 
-      tension: 280,
-      friction: 14,
-      mass: .05,
+      tension: 320,
+      friction: 15,
+      mass: 0.5,
     },
     delay: 0,
     trail: 0,
   });
 
-  const fadeIn = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: { duration: 50 }
-  });
-
   return (
-    <animated.div style={fadeIn}>
+    <animated.div>
       <Box
         sx={{
           position: 'fixed',
